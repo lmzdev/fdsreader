0b6edba3798a431785d2b906d2e65a9adfe5a7f2
Leonard Menzel <dev@leomenzel.de>
2023-03-15 22:43:55 +0100

    footer links

--- src/App.svelte
+++ src/App.svelte
@@ -1,70 +1,75 @@
 <script>
-  import {onMount} from 'svelte'
+  import { onMount } from "svelte";
   import Converter from "./lib/converter";
   import Footer from "./components/Footer.svelte";
   import Copy from "carbon-icons-svelte/lib/Copy.svelte";
   import Information from "carbon-icons-svelte/lib/Information.svelte";
-  import {zipurl, unzipurl} from 'zipurl'
+  import { zipurl, unzipurl } from "zipurl";
 
   let fdsOutput = "";
   let fdsInput = "";
-  let shareLink = ""
+  let shareLink = "";
 
   function clickConvert() {
     const c = new Converter(fdsInput);
     fdsOutput = c.toString();
     console.log(c.fdsObject);
-    document.getElementById('main-input').setAttribute('readonly', 'readonly')
-    shareLink = location.protocol + '//' + location.host + location.pathname + "?data=" + zipurl(fdsInput)
+    document.getElementById("main-input").setAttribute("readonly", "readonly");
+    shareLink =
+      location.protocol +
+      "//" +
+      location.host +
+      location.pathname +
+      "?data=" +
+      zipurl(fdsInput);
   }
 
   function selectLink() {
-    document.getElementById('share-link').focus()
+    document.getElementById("share-link").focus();
     // @ts-ignore
-    document.getElementById('share-link').select()
+    document.getElementById("share-link").select();
   }
 
   function copyContent() {
     navigator.clipboard.writeText(fdsOutput);
   }
   onMount(() => {
-    const params = new URLSearchParams(window.location.search)
-    const d = params.get('data')
+    const params = new URLSearchParams(window.location.search);
+    const d = params.get("data");
     if (d) {
       try {
-        fdsInput = String(unzipurl(d))
-        clickConvert()
+        fdsInput = String(unzipurl(d));
+        clickConvert();
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     }
-
-  })
+  });
 </script>
 
 <main>
   <div class="container-top container">
     <a href="/"><h1>FDS Reader</h1></a>
-    <div></div>
-    <p style="color:var(--secondary); display:none" >
+    <div />
+    <p style="color:var(--secondary); display:none">
       <Information style="margin-bottom: 4px" size={32} />
       Der FDS Reader wird nur im Browser ausgeführt und speichert keine Daten.
     </p>
-    
+
     <textarea
-      id='main-input'
+      id="main-input"
       placeholder="Inhalt der FDS-Datei hier einfügen..."
       rows={fdsOutput !== "" ? 3 : 6}
       bind:value={fdsInput}
       class="text-code"
     />
-    <button style="display: {fdsOutput === '' ? 'block' : 'none'}" on:click={clickConvert} disabled={!Converter.validate(fdsInput)}>
+    <button
+      style="display: {fdsOutput === '' ? 'block' : 'none'}"
+      on:click={clickConvert}
+      disabled={!Converter.validate(fdsInput)}
+    >
       Konvertieren
     </button>
-    
-
-
-    
   </div>
   <div class="container container-bot">
     <article
@@ -74,32 +79,29 @@
       <button
         on:click={copyContent}
         class="copy-btn outline secondary icon-btn"
-        data-tooltip="Text Kopieren" data-placement="left"><Copy size={20} /></button
+        data-tooltip="Text Kopieren"
+        data-placement="left"><Copy size={20} /></button
       >
       {fdsOutput}
 
       <footer>
-        <input 
-    readonly 
-    aria-readonly="true" 
-    id='share-link' 
-    type="text" 
-    value={shareLink} 
-    on:click={selectLink}
-    style="visibility: {fdsOutput !== '' ? 'visible' : 'collapse'}" />
+        <input
+          readonly
+          aria-readonly="true"
+          id="share-link"
+          type="text"
+          value={shareLink}
+          on:click={selectLink}
+          style="visibility: {fdsOutput !== '' ? 'visible' : 'collapse'}"
+        />
       </footer>
-
     </article>
     <div style="display: flex; max-width:100%">
-
-        <p class='fds-info'  >
-    <Information style="margin-bottom: 4px" size={24} />
-    Der FDS Reader wird nur im Browser ausgeführt und speichert keinerlei Daten.
-  </p>
-
+      <p class="fds-info">
+        <Information style="margin-bottom: 4px" size={24} />
+        Der FDS Reader wird nur im Browser ausgeführt und speichert keinerlei Daten.
+      </p>
     </div>
-    
-
   </div>
 
   <Footer />
@@ -107,12 +109,11 @@
 
 <style lang="css">
   .fds-info {
-    color:var(--secondary); 
+    color: var(--secondary);
     display: block;
     margin: 0px auto;
     font-size: smaller;
     text-align: center;
-
   }
   .icon-btn {
     font-family: var(--font-family);
@@ -124,7 +125,6 @@
     position: absolute;
     right: 36px;
     top: 66px;
-
   }
   #share-link {
     font-size: 0.95em;
@@ -149,5 +149,4 @@
     position: relative;
     padding: 0px 8px;
   }
-
 </style>
