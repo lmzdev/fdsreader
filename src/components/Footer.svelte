6ec3f71d01a8e4853e035f2a8f585bb46098de76
Leonard Menzel <dev@leomenzel.de>
2023-03-16 12:01:23 +0100

    add icons to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,20 @@
+<script>
+  import LogoGithub from "carbon-icons-svelte/lib/LogoGithub.svelte";
+  import Link from "carbon-icons-svelte/lib/Link.svelte";
+
+</script>
+
+
 <div class="footer">
-    <a class="f-item outline" href="https://feueron.de/feueron"> FeuerON </a>
+    <a class="f-item outline" href="/">Startseite</a>
+    <a class="f-item outline" href="https://feueron.de/feueron"><Link style="margin-bottom:2px" size={16} /> FeuerON </a>
     <a
       class="f-item outline"
       href="https://www.nlbk.niedersachsen.de/download/108403/FeuerON_-_FDS-Schnittstellenbeschreibung.pdf"
-    >
+    ><Link style="margin-bottom:2px" size={16} />
       FDS-Dokumentation
     </a>
-    <a class="f-item outline" href="https://github.com/lmzdev/fdsreader"> GitHub </a>
+    <a class="f-item outline" href="https://github.com/lmzdev/fdsreader"><LogoGithub style="margin-bottom:2px" size={16} /> GitHub </a>
   </div>
 
   <style>
@@ -14,7 +22,7 @@
     display: flex;
     max-width: 900px;
     margin: 20px auto;
-    padding: 6px 0px;
+    padding: 8px 0px;
     border-top: 1px solid var(--muted-color);
   }
   a.f-item {
